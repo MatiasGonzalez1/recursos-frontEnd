065572d694f3ca5ae42986e9e04fc5c750e0c17d
Matias Gonzalez <mng_epson@hotmail.com>
2023-01-26 22:27:45 -0300

    feat: add button add person to listado de personas/js/index.js

--- 15- Listado de personas/js/index.js
+++ 15- Listado de personas/js/index.js
@@ -1,1 +1,15 @@
-console.log('Listado de personas');
+const personas = [
+  new Persona('Juan', 'Perez'),
+  new Persona('Karla', 'Lara'),
+  new Persona('Maria', 'Suarez')
+]
+
+function mostrarPersonas(){
+  let texto = '';
+  for(let persona of personas){
+    texto += `<li> ${persona.nombre} ${persona.apellido}</li>`
+  }
+
+  let ul = document.getElementById('personas')
+  ul.innerHTML = texto;
+}
