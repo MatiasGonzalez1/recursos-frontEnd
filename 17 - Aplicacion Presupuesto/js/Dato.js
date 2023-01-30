378ce3ec8d93c631d9f5eede724b94dd6d8e12e0
Matias Gonzalez <mng_epson@hotmail.com>
2023-01-30 08:47:22 -0300

    feat: add class Datos to aplicacion presupuesto

---
+++ 17 - Aplicacion Presupuesto/js/Dato.js
@@ -0,0 +1,22 @@
+class Datos{
+  constructor(desripcion, valor){
+    this._descripcion = desripcion;
+    this._valor = valor;
+  }
+
+  get desripcion(){
+    return this._descripcion;
+  }
+
+  set desripcion(desripcion){
+    this._descripcion = desripcion;
+  }
+
+  get valor(){
+    return this._valor;
+  }
+
+  set valor(valor){
+    this._valor = valor;
+  }
+}
