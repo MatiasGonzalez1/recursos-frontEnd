d5337aa024070bd252c241f742bb6b3e20c7a6b9
Matias Gonzalez <mng_epson@hotmail.com>
2023-02-23 22:09:47 -0300

    feat: add eval function to 06 - Dark calculator

--- 06 - Dark calculator/index.js
+++ 06 - Dark calculator/index.js
@@ -1,5 +1,5 @@
 const display = document.querySelector('#display');
-const buttons = document.querySelector('button');
+const buttons = document.querySelectorAll('button');
 buttons.forEach((item) => {
   item.onclick = ()=>{
     if(item.id == 'clear'){
